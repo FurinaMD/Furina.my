19aeb177232c9ae4c0387eb32267a4eeba70e4d2
Furina By Doni <donialpian12345@gmail.com>
2024-11-12 20:59:01 +0700

    Add files via upload

---
+++ key1.js
@@ -0,0 +1,13 @@
+{
+  "allowedIps": ["172.18.0.6"],
+  "users": [
+    {
+      "username": "Doni",
+      "password": "doni123"
+    },
+    {
+      "username": "Nabila",
+      "password": "nabila123"
+    }
+  ]
+}
